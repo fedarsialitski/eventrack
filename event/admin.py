5765e5d529b599671b7bbcbd47b3631ad29f8c3e
FedorSelitsky <FedorSelitsky@gmail.com>
2017-07-23 20:00:28 +0300

    Add Artist, Event, Venue models

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artist, Event, Venue
 
-# Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Event)
+admin.site.register(Venue)
--- event/models.py
+++ event/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+    events = models.ManyToManyField(
+        'event.Event',
+        related_name='artists',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    datetime = models.DateTimeField()
+
+    venue = models.ForeignKey(
+        'event.Venue',
+        related_name='events',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
