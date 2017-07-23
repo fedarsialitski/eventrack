a20bd15c0dfb226b8908d0eb5cee175346dca4f9
FedorSelitsky <FedorSelitsky@gmail.com>
2017-07-23 21:30:03 +0300

    Customize Artist, Event, Venue admin forms

--- event/admin.py
+++ event/admin.py
@@ -2,6 +2,45 @@ from django.contrib import admin
 from .models import Artist, Event, Venue
 
 
-admin.site.register(Artist)
-admin.site.register(Event)
-admin.site.register(Venue)
+class ArtistEventsInline(admin.TabularInline):
+    model = Artist.events.through
+    verbose_name_plural = 'Events'
+    extra = 1
+
+
+class EventArtistsInline(admin.TabularInline):
+    model = Event.artists.through
+    verbose_name_plural = 'Artists'
+    extra = 1
+
+
+class EventInline(admin.StackedInline):
+    model = Event
+    extra = 1
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields': ['name']}),
+    ]
+    inlines = [ArtistEventsInline]
+    search_fields = ['name']
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [EventArtistsInline]
+    list_display  = ['title', 'datetime', 'venue']
+    list_filter   = ['title', 'datetime', 'venue__name']
+    search_fields = ['title', 'datetime', 'venue__name']
+
+
+class VenueAdmin(admin.ModelAdmin):
+    inlines = [EventInline]
+    list_display  = ['name', 'city', 'country']
+    list_filter   = ['name', 'city', 'country']
+    search_fields = ['name', 'city', 'country']
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Venue, VenueAdmin)
