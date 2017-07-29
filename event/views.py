f892cbc03565f06075184f00e4749c4ec4c02214
FedorSelitsky <FedorSelitsky@gmail.com>
2017-07-29 17:05:39 +0300

    Add Artist upcoming and past events

--- event/views.py
+++ event/views.py
@@ -33,12 +33,15 @@ class ArtistView(generic.ListView):
 class ArtistDetailView(generic.DetailView):
     model = Artist
 
-    def get_object(self):
-        """
-        Return the arist detail information
-        """
-        object = super(ArtistDetailView, self).get_object()
-        return object
+    def get_context_data(self, **kwargs):
+        context = super(ArtistDetailView, self).get_context_data(**kwargs)
+        context['upcoming_events'] = self.object.events.filter(
+            datetime__gte=timezone.now()
+        ).order_by('datetime')
+        context['past_events'] = self.object.events.filter(
+            datetime__lte=timezone.now()
+        ).order_by('-datetime')
+        return context
 
 
 class EventView(generic.ListView):
