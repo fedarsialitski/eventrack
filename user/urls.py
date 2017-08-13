d14a154b10ae8796d6187233d4bc956dc627b958
FedorSelitsky <FedorSelitsky@gmail.com>
2017-08-13 18:01:07 +0300

    Refactor urls

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^signin/$', views.signin, name='signin'),
-    url(r'^signout/$', views.signout, name='signout'),
+    url(r'^signup/$',  views.signup,                name='signup'),
+    url(r'^signin/$',  views.signin,                name='signin'),
+    url(r'^signout/$', views.signout,               name='signout'),
 ]
