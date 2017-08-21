5ab4bc5106ce1ce54ce13ef5009c83d185a86b72
FedorSelitsky <FedorSelitsky@gmail.com>
2017-08-21 17:08:52 +0300

    Add Bookmark button

--- event/static/event/scripts/event.js
+++ event/static/event/scripts/event.js
@@ -4,12 +4,24 @@ $(document).on('click', 'div.item-action a', function () {
 });
 
 $(document).on('click', '.btn-favorite', function () {
-  var url = $(this).data('url');
+  var url = $(this).data('url'),
+      button = $(this).children('i'),
+      className = button[0].classList[1];
 
   $.ajax({
     url: url,
     dataType: 'json',
     success: function () {
+      switch (className) {
+        case 'fa-star':
+          button.removeClass('fa-star');
+          button.addClass('fa-star-o');
+          break;
+        case 'fa-star-o':
+          button.removeClass('fa-star-o');
+          button.addClass('fa-star');
+          break;
+      }
     }
   });
 });
