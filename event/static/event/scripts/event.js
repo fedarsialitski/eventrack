328502e46d8e25667637cdfe6aac42347d05579d
FedorSelitsky <FedorSelitsky@gmail.com>
2017-08-22 16:08:15 +0300

    Add removing Artist, Event and Venue from bookmarks

--- event/static/event/scripts/event.js
+++ event/static/event/scripts/event.js
@@ -4,23 +4,32 @@ $(document).on('click', 'div.item-action a', function () {
 });
 
 $(document).on('click', '.btn-favorite', function () {
-  var url = $(this).data('url'),
+  var id = $(this).data('id'),
+      url = $(this).data('url'),
       button = $(this).children('i'),
-      className = button[0].classList[1];
+      className = button[0].classList[1],
+      pathName = window.location.pathname;
 
   $.ajax({
     url: url,
     dataType: 'json',
     success: function () {
-      switch (className) {
-        case 'fa-star':
-          button.removeClass('fa-star');
-          button.addClass('fa-star-o');
-          break;
-        case 'fa-star-o':
-          button.removeClass('fa-star-o');
-          button.addClass('fa-star');
-          break;
+      if (pathName === '/profile/') {
+        var elements = document.querySelectorAll("#" + id);
+        Array.prototype.forEach.call(elements, function(node) {
+          node.parentNode.removeChild(node);
+        });
+      } else {
+        switch (className) {
+          case 'fa-star':
+            button.removeClass('fa-star');
+            button.addClass('fa-star-o');
+            break;
+          case 'fa-star-o':
+            button.removeClass('fa-star-o');
+            button.addClass('fa-star');
+            break;
+        }
       }
     }
   });
